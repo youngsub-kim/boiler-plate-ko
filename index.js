f5680697fd70e3f13ad45262d9a1d0ab79d482c8
youngsub <youngsubi12@naver.com>
2020-09-25 14:18:23 +0900

    some init msg

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://youngsub:abcd1234@boilerplate.znfiu.mongodb.net/youngsub?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+  app.get('/', (req, res) => res.send('Hello World!'))
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
