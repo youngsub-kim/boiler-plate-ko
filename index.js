3a67da278c80eb4b82dcd82647218b85039d4e1f
youngsub <youngsubi12@naver.com>
2020-09-29 11:09:27 +0900

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 
@@ -17,9 +18,9 @@ app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true, 
-  useCreateIndex: true, 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 })
   .then(() => console.log('MongoDB Connected...'))
@@ -27,8 +28,9 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!!!!!!!!!!!!'))
 
+app.get('/api/hello', (req, res) => res.send('Hello World!!!!!!!!!!!!'))
 
-app.post('/register', (req, res) => {
+app.post('api/users/register', (req, res) => {
 
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
@@ -43,7 +45,7 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('api/users/login', (req, res) => {
 
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -68,14 +70,43 @@ app.post('/login', (req, res) => {
     if (err) return res.status(400).send(err);
 
     // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
-      res.cookie("x_auth", user.token)
+    res.cookie("x_auth", user.token)
       .status(200)
-      .json({ loginSuccess: true, userId: user_id })
+      .json({ loginSuccess: true, userId: user._id })
   })
 })
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('api/users/auth', auth, (req, res) => {
+
+  //여기까지 미들웨어로 통과해 왔다는 얘기는 Authentication이 true라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
 })
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" },
+    (err, user) => {
+      if (err) return res.json({ success: false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
--- models/User.js
+++ models/User.js
@@ -63,7 +63,7 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
     //plainPassword 1234567     암호화된 비밀번호 $2b$10$fMXbSOjx6NTzaJ/PHNgIXuqIZ6TOeaV/vYhJOeK5Q6z7MX5hyq8kG
     bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
         if (err) return cb(err);
-        cb(null, isMatch)
+        cb(null, isMatch);
     })
 }
 
@@ -82,7 +82,20 @@ userSchema.methods.generateToken = function (cb) {
     })
 }
 
+userSchema.static.findByToken = function (token, cb) {
+    var user = this;
 
+    //토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id": decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+
+}
 
 
 
